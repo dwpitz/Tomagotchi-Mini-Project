a4ce5c26307df83db4a44e413dc8eaf2edc80b37
David Pitz <dw_pitz@yahoo.com>
2019-10-01 15:32:46 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+console.log('hello');
+
+//class Tomagatchi {
+	//if somethign is not a property of the tomagatchi it does not go in the tomagatchi class
+	// eat:
+	// die:
+	// plays:
+	// sleep:
+
+
+//}
+
+// const game = {
+	//timer method - only one interval in the game.  DO NOT have more than one timer.
+
+// }
+
+// $().on('click', () => {
+// 	//listeners can be in the global scope
+// }
+// )
+
