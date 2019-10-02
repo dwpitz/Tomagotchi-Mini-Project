8e0387bbbb3ba3879ed9314fb7d542234a2c1af2
David Pitz <dw_pitz@yahoo.com>
2019-10-02 10:05:57 -0500

    some game pseudo coding

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 console.log('hello');
 //	if somethign is not a property of the tomagatchi it does not go in the tomagatchi class
+//Tomagatchi Class
 class Tomagatchi {
 	constructor(){
 		this.age: 0;
@@ -8,19 +9,71 @@ class Tomagatchi {
 		this.sleepiness: 1;
 	},
 	name:
-
-
 }
 
-//timer method - only one interval in the game.  DO NOT have more than one timer.
-const game = {
-	start(){
-		new 
+//Game Play Object
+lifeCycle {
+	birth(){
+		//Jack Is Seen On The Screen
+		//Stats are displayed
+		//Buttons to take care of Jack are displayed
+		//Rules Are Displayed.
+		//name your pet.
 	},
+	setTimer(){
+		//This is should count up.
+		//1 year = 2 mins
+	},
+
+	age(){
+
+	},
+
+	hunger(){
+
+	},
+
+	sleepiness(){
+
+	},
+
+	boredom(){
+
+	},
+
+	death(){
+
+	}
+
 }
+
+//Listeners Object
+// 	start(){
+// 		const jack = new Tomagatchi() 
+// 	},
+	//setInterval() <== How does this timer work.  Is it counting down?  Counting up?  
+//}
 
 // $().on('click', () => {
 // 	//listeners can be in the global scope
 // }
 // )
+
+//timer method - only one interval in the game.  DO NOT have more than one timer.
+// const game = {
+
+//Rules:
+//When hunger hits 5, you have to feed it.  
+//When sleepiness hits 5, you have to turn out the lights
+//When Boredom hits 5, you have to play with it.  
+//If Hunger hits 10 || Sleepiness hits 10 || Boredom hits 10 <===Tomagatchi dies
+//When age hits
+
+//The Timer:
+//Age = 1 year every 30 secs.
+//hunger = Hungry every 5 secs.
+//sleepiness = sleepy every 15 secs.
+//Boredom = every 10 secs
+
+
 
